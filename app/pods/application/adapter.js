0c381df952f20f902ebd78ddc8c19e6194085d30
Sammy Nave <sammy.nave@meyouhealth.com>
2016-06-14 22:39:30 -0400

    save point

--- app/pods/application/adapter.js
+++ app/pods/application/adapter.js
@@ -1,7 +1,9 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import config from 'pick-another-web/config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
+  authorizer: 'authorizer:oauth2',
   host: config.apiEndpoint,
   namespace: 'api'
 });
